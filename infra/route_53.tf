6e40708d24ddd1fcec9ff5653f91e974a69cd80a
RodneyPerez <Rodney.the.programmer@gmail.com>
2020-01-10 15:57:26 -0700

    Organizing the route 53 resources

--- infra/route_53.tf
+++ infra/route_53.tf
@@ -2,13 +2,6 @@ resource "aws_route53_zone" "main" {
   name = var.domain_name
 }
 
-resource "aws_acm_certificate" "main" {
-  domain_name               = var.domain_name
-  subject_alternative_names = ["*.${var.domain_name}"]
-  validation_method         = "DNS"
-  tags                      = var.main_tags
-}
-
 resource "aws_route53_record" "main" {
   zone_id = aws_route53_zone.main.zone_id
   name    = var.domain_name
@@ -21,18 +14,14 @@ resource "aws_route53_record" "main" {
   }
 }
 
-resource "aws_route53_record" "validation" {
-  name    = aws_acm_certificate.main.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.main.domain_validation_options[0].resource_record_type
+resource "aws_route53_record" "redirect" {
   zone_id = aws_route53_zone.main.zone_id
-  records = [aws_acm_certificate.main.domain_validation_options[0].resource_record_value]
-  ttl     = "60"
-}
+  name    = var.www_domain_name
+  type    = "A"
 
-resource "aws_acm_certificate_validation" "default" {
-  certificate_arn = aws_acm_certificate.main.arn
-  validation_record_fqdns = [
-    aws_route53_record.validation.fqdn,
-  ]
+  alias {
+    name                   = aws_route53_zone.main.name
+    zone_id                = aws_route53_record.main.zone_id
+    evaluate_target_health = false
+  }
 }
-
