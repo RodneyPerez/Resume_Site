c4c1275a132760ca36e3a9b8fdf79bac11ec1a24
RodneyPerez <Rodney.the.programmer@gmail.com>
2020-01-10 15:28:34 -0700

    Renaming locals into vars

--- infra/locals.tf
+++
@@ -1,9 +0,0 @@
-variable "www_domain_name" {}
-variable "domain_name" {}
-variable "secret" {}
-variable "main_tags" {
-  type = "map"
-}
-variable "redirect_tags" {
-  type = "map"
-}
---
+++ infra/vars.tf
@@ -0,0 +1,15 @@
+variable "www_domain_name" {
+  type = "string"
+}
+variable "domain_name" {
+  type = "string"
+}
+variable "secret" {
+  type = "string"
+}
+variable "main_tags" {
+  type = "map"
+}
+variable "redirect_tags" {
+  type = "map"
+}
